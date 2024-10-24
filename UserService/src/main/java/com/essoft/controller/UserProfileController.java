7f742ef8fe0986a1ea8c66d183990751f1d4d35e
ErtanSidar <essoft3@gmail.com>
2024-10-24 14:44:28 +0300

    add redis for caching

--- UserService/src/main/java/com/essoft/controller/UserProfileController.java
+++ UserService/src/main/java/com/essoft/controller/UserProfileController.java
@@ -27,4 +27,9 @@ public class UserProfileController {
     public ResponseEntity<List<UserProfile>> getAll() {
         return ResponseEntity.ok(userProfileService.getAll());
     }
+
+    @GetMapping
+    public ResponseEntity<String> upperName(String name) {
+        return ResponseEntity.ok(userProfileService.upperName(name));
+    }
 }
--- UserService/src/main/java/com/essoft/service/UserProfileService.java
+++ UserService/src/main/java/com/essoft/service/UserProfileService.java
@@ -4,6 +4,8 @@ import com.essoft.document.UserProfile;
 import com.essoft.dto.request.CreateUserRequestDto;
 import com.essoft.repository.UserProfileRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserProfileService {
     private final UserProfileRepository userProfileRepository;
+    private final CacheManager cacheManager;
 
     public void createUser(CreateUserRequestDto createUserRequestDto) {
         userProfileRepository.save(UserProfile.builder()
@@ -21,7 +24,19 @@ public class UserProfileService {
                 .build());
     }
 
+
     public List<UserProfile> getAll() {
         return userProfileRepository.findAll();
+    }
+
+    // this is an example for redis
+    @Cacheable("upper-name")
+    public String upperName(String name) {
+        String result = name.toUpperCase();
+        return result;
+    }
+
+    public void clearCache() {
+        cacheManager.getCache("upper-name").clear();
     }
 }
